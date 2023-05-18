88c3d0d52c9374546de64ed0ba5df5f298c30142
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-05-18 16:06:36 -0300

    React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React from 'react';
 
+import { BrowserRouter } from 'react-router-dom'
 import Login from './pages/Login';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <Login />
       <GlobalStyles />
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Error/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from '../../styles/GlobalStyles';
+
+export default function Error() {
+  return (
+    <Container>
+      <h1>This page does not exist.</h1>
+    </Container>
+  );
+}
