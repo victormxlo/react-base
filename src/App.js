d554c734b079344344396bf30b200dd4c5fc02e6
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-05-18 18:50:57 -0300

    React Toastify

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-
 import { Router } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+
 import history from './services/history';
 import Login from './pages/Login';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 
 function App() {
+  toast.success('Success message', {
+    position: toast.POSITION.TOP_RIGHT
+  })
+
   return (
     <Router history={history}>
       <Header />
       <Login />
       <GlobalStyles />
+      <ToastContainer autoClose={3000}/>
     </Router>
   );
 }
