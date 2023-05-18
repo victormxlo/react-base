755f0d2325a2c5b5f5b12172dba306b6a61480c6
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-05-18 17:00:54 -0300

    Private Route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Error from '../pages/Error';
 
 export default function Routes() {
   return (
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="*" component={Error} />
+        <MyRoute exact path="/" component={Login} />
+        <MyRoute path="*" component={Error} />
       </Switch>
   );
 }
