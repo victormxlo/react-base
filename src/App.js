1b3e744e56172c294e6568f70dd712699b0149d1
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-05-19 11:12:38 -0300

    Corrected URL path

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import { Router } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 
 import history from './services/history';
-import Login from './pages/Login';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
+import Routes from './routes/index';
 
 function App() {
-  toast.success('Success message', {
-    position: toast.POSITION.TOP_RIGHT
-  })
 
   return (
     <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
       <ToastContainer autoClose={3000}/>
     </Router>
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,18 +2,8 @@ import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragraph } from './styled';
-import axios from '../../services/axios';
 
 export default function Login() {
-  React.useEffect( () => {
-    async function getData() {
-      const response = await axios.get('/clients');
-      const { data } = response;
-    }
-
-    getData();
-  }, []);
-
   return (
     <Container>
       <Title>Title</Title>
