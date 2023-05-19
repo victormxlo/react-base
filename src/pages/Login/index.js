d8e2f57b0ba43964c3e2a27f5d7f1d7d5cc653e9
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-05-19 09:05:24 -0300

    Axios

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragraph } from './styled';
+import axios from '../../services/axios';
 
 export default function Login() {
+  React.useEffect( () => {
+    async function getData() {
+      const response = await axios.get('/clients');
+      const { data } = response;
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
-      <Title>Title<small>Small</small></Title>
+      <Title>Title</Title>
       <Paragraph>Lorem ipsum dolor sit amet.</Paragraph>
       <button type="button">Send</button>
     </Container>
